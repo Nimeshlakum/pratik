e68048497c807eb609d88dc9421a3bd524181839
nimeshlakum <lakumnimesh24@gamil.com>
2024-05-02 16:09:33 +0530

    new change

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path , include
+from django.conf.urls.static import static
 
 #custom 
 
@@ -24,4 +26,4 @@ from myapp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('myapp.url')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
