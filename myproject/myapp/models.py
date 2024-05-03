eff4ff2ecdfbdaf548cdb0785d08af733fc414d5
nimeshlakum <lakumnimesh24@gamil.com>
2024-05-03 10:57:44 +0530

    new image of product

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -4,10 +4,12 @@ from django.db import models
 # Create your models here.
 
 class product(models.Model):
-    product_name = models.CharField(max_length=30)
-    product_price = models.DecimalField(max_digits=10,decimal_places=2)
-    product_discount = models.DecimalField(max_digits=10,decimal_places=2)
-    product_image = models.ImageField(upload_to="p_image",max_length=50)
+    product_name = models.CharField(max_length=30,null=True)
+    product_type = models.CharField(max_length=30,null=True)
+    product_price = models.DecimalField(max_digits=10,decimal_places=2,null=True)
+    product_discount = models.DecimalField(max_digits=3,decimal_places=0,null=True)
+    product_image = models.ImageField(upload_to="p_image",max_length=50,null=True)
+    product_main = models.IntegerField(default=0,null=True)
 
     class Meta:
         db_table = 'product'
