a2a6813788d33880065933de825ae6e1558d1c56
nimeshlakum <lakumnimesh24@gamil.com>
2024-05-04 13:19:20 +0530

    new files

--- myproject/myapp/url.py
+++ myproject/myapp/url.py
@@ -7,9 +7,20 @@ urlpatterns = [
 
     path ("",main,name="main"),
     path ("home/",main,name="main"),
-    path ("product_page/",product_page,name="product_page"),
-    path ("wishlist_page/",product_page,name="wishlist_page"),
-    path ("cart_page/",product_page,name="cart_page")
 
+    path("login_page/",login_page),
+    path("login/",user_login),
+
+    path("logout/",user_logout),
+
+    path("registration_page/",registration),
+    path("registration/",signup),
+
+    path ("product_page/",product_page,name="product_page"),
+    path ("wishlist_page/",wishlist_page,name="wishlist_page"),
+    path ("cart_page/",cart_page,name="cart_page"),
+
+    path ("wishlist/",wishlist,name="wishlist"),
+    path ("cart/",cart,name="cart")
 
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render 
+from django.shortcuts import render , HttpResponse , redirect
+from django.contrib.auth import authenticate , login , logout
 from .models import *
+from django.contrib.auth.models import *
+from django.contrib import messages
 
-# Create your views here.
+
+def signup(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password')   
+        
+        try:
+            myuser=User.objects.create_user(username,email,pass1)
+            myuser.save()
+            messages.success(request,"User is Created Please Login")
+            return render(request,"login.html")
+        
+        except:
+            pass
+
+    return render(request,"registration.html")
+
+def user_login(request):
+    if request.method=="POST":        
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"Login Successful")
+            return redirect('/home/')
+        else:
+            messages.error(request,"Invalid Credentials")
+            return render(request,'login.html')  
+               
+    return render(request,'login.html')
+
+def user_logout(request):
+    logout(request)
+    return render(request,'login.html')
+
+def registration(request):
+    return render(request,'registration.html')
+
+def login_page(request):
+    return render(request,'login.html')
 
 def main(request):
     p_deatil = product.objects.all() 
@@ -22,11 +66,23 @@ def wishlist_page(request):
     all = {
         'p_deatil':p_deatil,
     }
-    return render(request,'product.html',all)
+    return render(request,'wishlist.html',all)
 
 def cart_page(request):
     p_deatil = product.objects.all() 
     all = {
         'p_deatil':p_deatil,
     }
-    return render(request,'product.html',all)
+    return render(request,'cart.html',all)
+
+def wishlist(request):
+    if request.user.is_authenticated:
+        return redirect('/wishlist_page/')
+    else:
+        return redirect('/login_page/')
+
+def cart(request):
+    if request.user.is_authenticated:
+        return redirect('/cart_page/')
+    else:
+        return redirect('/login_page/')
